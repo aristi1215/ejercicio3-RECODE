5e4e9fabfc447e5b62f215ef48ea700f0c18c7cf
aristi1215 <juanarieda@gmail.com>
2023-12-28 07:54:18 -0500

    First commit

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+// Animacion cuando se recargue la pagina
+
+// document.addEventListener("DOMContentLoaded",function(){
+//     let title = documet.getElementById("title")
+
+//     title.classList.add("animate-on-load")
+
+// })
+
+document.addEventListener('DOMContentLoaded', function () {
+    var title = document.getElementById('title');
+    title.classList.add('animated'); // Agrega la clase CSS para iniciar la animación
+  });
